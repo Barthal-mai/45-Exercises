6948f7dba19c464530e8d02d6c185948d6a344c3
Barthal-mai <149488778+Barthal-mai@users.noreply.github.com>
2024-07-29 11:32:06 +0500

    Getting Started Exercises with TypeScript and Node.js
    Getting Started Exercises with TypeScript and Node.js

---
+++ Q39.js
@@ -0,0 +1,8 @@
+function city_country(city, country) {
+    return "".concat(city, ", ").concat(country);
+}
+// Example usage:
+var cityName = "Lahore";
+var countryName = "Pakistan";
+var result = city_country(cityName, countryName);
+console.log(result); // Outputs: Lahore, Pakistan
